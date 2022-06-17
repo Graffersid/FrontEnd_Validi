36c2a348b1e5782854e8a750844d4d4b2b120193
Ajay06Meena <105280680+Ajay06Meena@users.noreply.github.com>
2022-06-17 19:03:38 +0530

    login page design

--- src/components/landingPage/Login.jsx
+++ src/components/landingPage/Login.jsx
@@ -1,26 +1,37 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faLock } from '@fortawesome/free-solid-svg-icons'
 
 const Login = () => {
     const [state, setState] = useState({
-        email:'',
-        password:'',
+        email: '',
+        password: '',
     })
 
     const handleChange = (e) => {
-        setState({...state,[e.target.name]:e.target.value})
+        setState({ ...state, [e.target.name]: e.target.value })
     }
     return (
         <div className="container">
             <div className="card" style={{ width: "18rem", display: 'inline-block', marginTop: '80px' }}>
                 <div className="card-body">
                     <form>
-                        <div className="form-group">
-                            <label htmlFor="exampleInputEmail1">email</label>
-                            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" name='email' value={state.email} onChange={handleChange} required />
+                        <div className="form-group input-group">
+                            <div className="input-group-prepend">
+                                <span className="input-group-text">
+                                    <FontAwesomeIcon icon={faEnvelope} size="2x" />
+                                </span>
+                            </div>
+                            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email" name='email' value={state.email} onChange={handleChange} required />
                         </div>
-                        <div className="form-group">
-                            <label htmlFor="exampleInputPassword1">Password</label>
+                        <div className="form-group input-group">
+                            <div className="input-group-prepend">
+                                <span className="input-group-text">
+                                    <FontAwesomeIcon icon={faLock} size="2x" />
+                                </span>
+                            </div>
                             <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" name='password' value={state.password} onChange={handleChange} required />
+
                         </div>
                         <button type="submit" className="btn btn-primary">Submit</button>
                     </form>
